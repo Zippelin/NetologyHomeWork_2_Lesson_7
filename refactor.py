cccf0d8c34659503f7172b1a90e2dc7122af8fda
denis <dens.andreev@cti.ru>
2021-04-07 18:48:33 +0300

    changed class

--- refactor.py
+++ refactor.py
@@ -1,6 +1,7 @@
 import email
 import smtplib
 import imaplib
+import requests
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -9,48 +10,55 @@ class MailWorker:
     __BASE_URL = "gmail.com"
     __GMAIL_SMTP = '.'.join(('smtp', __BASE_URL))
     __GMAIL_IMAP = '.'.join(('imap', __BASE_URL))
-    _SMTP_PORT = 587
+    __SMTP_PORT = 587
+    __IMAP_SSL_PORT = 993
+
+    __CONNECTORS = {
+        'send': smtplib.SMTP,
+        'receive': imaplib.IMAP4_SSL
+    }
 
     def __init__(self, login, password):
         self.__login = login
         self.__password = password
 
-    def send_message(self, text, to, subject=None):
-        with smtplib.SMTP(self.__GMAIL_SMTP, self._SMTP_PORT) as smtp_connector:
-            message = MIMEMultipart(From=self.__login, To=to, Subject=subject)
-            message.attach(MIMEText(text, _charset='utf-8'))
-
-            # identify ourselves to smtp gmail client
-            smtp_connector.ehlo()
-            # secure our email with tls encryption
-            smtp_connector.starttls()
-            # re-identify ourselves as an encrypted connection
-            smtp_connector.ehlo()
-
+    def process_message(self, method=None, message=None, filter_='ALL'):
+        port = self.__IMAP_SSL_PORT
+        host = self.__GMAIL_IMAP
+        if method == 'send':
+            port = self.__SMTP_PORT
+            host = self.__GMAIL_SMTP
+        with self.__CONNECTORS[method](host=host, port=port) as connector:
+            if method == 'send':
+                connector.ehlo()
+                connector.starttls()
+                connector.ehlo()
             try:
-                smtp_connector.login(self.__login, self.__password)
-            except smtplib.SMTPAuthenticationError as e:
-                print(e)
-                return
-            smtp_connector.sendmail(self.__login, smtp_connector, message.as_string())
-
-    def receive_message(self, filter='ALL'):
-        with imaplib.IMAP4_SSL(self.__GMAIL_IMAP) as imap_connector:
-            try:
-                imap_connector.login(self.__login, self.__password)
+                connector.login(self.__login, self.__password)
             except Exception as e:
                 print(e)
                 return
-            imap_connector.list()
-            imap_connector.select("inbox")
-            _, data = imap_connector.uid('search', None, '(HEADER Subject "%s")' % filter)
+            if method == 'send':
+                connector.sendmail(self.__login, connector, message.as_string())
+                return
+            connector.list()
+            connector.select("inbox")
+            _, data = connector.uid('search', None, '(HEADER Subject "%s")' % filter_)
             assert data[0], 'There are no letters with current header'
             latest_email_uid = data[0].split()[-1]
-            _, data = imap_connector.uid('fetch', latest_email_uid, '(RFC822)')
+            _, data = connector.uid('fetch', latest_email_uid, '(RFC822)')
             raw_email = data[0][1]
             return email.message_from_string(raw_email)
+
+    def send(self, text, to, subject=None):
+        message = MIMEMultipart(From=self.__login, To=to, Subject=subject)
+        message.attach(MIMEText(text, _charset='utf-8'))
+        self.process_message('send', message=message)
+
+    def receive(self, filter='ALL'):
+        self.process_message('receive', filter)
 
 
 if __name__ == '__main__':
     mail = MailWorker('text', 'text')
-    mail.send_message('hello', 'aa@22.com')
+    mail.send('hello', 'aa@22.com')
